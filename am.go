6154cd75a4c48bc2226a5f572831d570e7ec545c
Raido Pahtma <raidopahtma@gmail.com>
2015-08-04 17:34:50 +0300

    go-flags marshalling functions for hex values.

--- am.go
+++ am.go
@@ -3,12 +3,35 @@
 
 package sfconnection
 
+import "encoding/hex"
 import "strconv"
 import "fmt"
 
+type HexByte byte
+type HexString []byte
 type AMAddr uint16
 type AMID uint8
 type AMGroup uint8
+
+func (self *HexByte) UnmarshalFlag(value string) error {
+	data, err := strconv.ParseUint(value, 16, 8)
+	*self = HexByte(data)
+	return err
+}
+
+func (self HexByte) MarshalFlag() (string, error) {
+	return fmt.Sprintf("%02X", self), nil
+}
+
+func (self *HexString) UnmarshalFlag(value string) error {
+	data, err := hex.DecodeString(value)
+	*self = data
+	return err
+}
+
+func (self HexString) MarshalFlag() (string, error) {
+	return hex.EncodeToString(self), nil
+}
 
 func (self *AMAddr) UnmarshalFlag(value string) error {
 	data, err := strconv.ParseUint(value, 16, 16)
