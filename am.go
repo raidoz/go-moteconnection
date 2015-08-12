a71edd622a95dd3ed2ffaf56dcd27393e8c0385d
Raido Pahtma <raidopahtma@gmail.com>
2015-08-12 18:04:19 +0300

    Added string methods.

--- am.go
+++ am.go
@@ -13,6 +13,10 @@ type AMAddr uint16
 type AMID uint8
 type AMGroup uint8
 
+func (self HexByte) String() string {
+	return fmt.Sprintf("%02X", byte(self))
+}
+
 func (self *HexByte) UnmarshalFlag(value string) error {
 	data, err := strconv.ParseUint(value, 16, 8)
 	*self = HexByte(data)
@@ -20,7 +24,11 @@ func (self *HexByte) UnmarshalFlag(value string) error {
 }
 
 func (self HexByte) MarshalFlag() (string, error) {
-	return fmt.Sprintf("%02X", self), nil
+	return self.String(), nil
+}
+
+func (self HexString) String() string {
+	return hex.EncodeToString(self)
 }
 
 func (self *HexString) UnmarshalFlag(value string) error {
@@ -30,7 +38,11 @@ func (self *HexString) UnmarshalFlag(value string) error {
 }
 
 func (self HexString) MarshalFlag() (string, error) {
-	return hex.EncodeToString(self), nil
+	return self.String(), nil
+}
+
+func (self AMAddr) String() string {
+	return fmt.Sprintf("%04X", uint16(self))
 }
 
 func (self *AMAddr) UnmarshalFlag(value string) error {
@@ -40,7 +52,11 @@ func (self *AMAddr) UnmarshalFlag(value string) error {
 }
 
 func (self AMAddr) MarshalFlag() (string, error) {
-	return fmt.Sprintf("%04X", self), nil
+	return self.String(), nil
+}
+
+func (self AMID) String() string {
+	return fmt.Sprintf("%02X", uint8(self))
 }
 
 func (self *AMID) UnmarshalFlag(value string) error {
@@ -50,7 +66,11 @@ func (self *AMID) UnmarshalFlag(value string) error {
 }
 
 func (self AMID) MarshalFlag() (string, error) {
-	return fmt.Sprintf("%02X", self), nil
+	return self.String(), nil
+}
+
+func (self AMGroup) String() string {
+	return fmt.Sprintf("%02X", uint8(self))
 }
 
 func (self *AMGroup) UnmarshalFlag(value string) error {
@@ -60,5 +80,5 @@ func (self *AMGroup) UnmarshalFlag(value string) error {
 }
 
 func (self AMGroup) MarshalFlag() (string, error) {
-	return fmt.Sprintf("%02X", self), nil
+	return self.String(), nil
 }
